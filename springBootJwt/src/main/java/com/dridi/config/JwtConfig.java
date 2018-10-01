a32dc7a1b67caa4f9cd4f6988aab8691f6ba7601
ramzi dridi <NST-USER-01@DESKTOP-NNSHCNA>
2018-10-01 12:19:50 +0100

    jwtConfig

--- springBootJwt/src/main/java/com/dridi/config/JwtConfig.java
+++ springBootJwt/src/main/java/com/dridi/config/JwtConfig.java
@@ -1,5 +1,22 @@
 package com.dridi.config;
 
-public class JwtConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+@Configuration
+@EnableAuthorizationServer
+public class JwtConfig  extends AuthorizationServerConfigurerAdapter{
+	
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Value("$varun.oauth.tokenTimeout:3600")
+	private int expiration;
 }
