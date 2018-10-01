e969f0a31b31b9a82dc911edf56cc41dc5c80aa5
NST-USER-01 <NST-USER-01@DESKTOP-NNSHCNA>
2018-10-01 13:31:23 +0100

    add controller and app classes

---
+++ springBootJwt/src/main/java/com/dridi/controllers/MyController.java
@@ -0,0 +1,15 @@
+package com.dridi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@RequestMapping(value="/hello")
+	public String hello() {
+		return "Hello JWt";
+		
+	}
+
+}
